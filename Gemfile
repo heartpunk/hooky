8aa62f088eb2b3e39073e2221cbdad60682ef2f3
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2013-01-01 18:57:43 -0800

    first version of hooky.  polls a thing and outputs the response.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+
+gem 'daemons'
+gem 'rest-client'
+gem 'json'
---
+++ hooky.rb
@@ -0,0 +1,23 @@
+require 'daemons'
+require 'rest-client'
+require 'json'
+require 'pp'
+
+SLEEP_INTERVAL = 30
+API_KEY = ENV['STACK_EXCHANGE_KEY']
+
+loop do
+  params = {
+    :params => {
+      key: API_KEY,
+      order: 'desc',
+      sort: 'creation',
+      tagged: 'ruby-on-rails',
+      site: 'stackoverflow'
+    }
+  }
+  response = RestClient.get 'http://api.stackexchange.com/2.1/search', params
+
+  puts JSON.pretty_generate(JSON.parse(response.body))
+  sleep SLEEP_INTERVAL
+end
